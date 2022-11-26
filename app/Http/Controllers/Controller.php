906352b14d8ad3dc1d73383c4cdf1f3dfd18a8c8
ahmaruff <ahmadmaruf2701@gmail.com>
2022-11-26 12:41:16 +0700

    create base resource not found function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Response;
 
 class Controller extends BaseController
 {
-    //
+    public function resourceNotFound(array $resource)
+    {
+        $response = [
+            'status' => 'fail',
+            'code' => 404,
+            'data' => $resource
+        ];
+        return response()->json($response,Response::HTTP_NOT_FOUND);
+    }
 }
