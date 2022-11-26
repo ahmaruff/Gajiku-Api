55c5049959c4679d20b67a84958ba47f0e647348
ahmaruff <ahmadmaruf2701@gmail.com>
2022-11-26 14:04:01 +0700

    change User model to implements JWT Auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,21 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Lumen\Auth\Authorizable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model implements JWTSubject, AuthorizableContract, AuthenticatableContract
 {
-    use Authenticatable, Authorizable, HasFactory;
+    use HasFactory, Authorizable, Authenticatable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
+      * The attributes that are mass assignable.
+      *
+      * @var string[]
+    */
     protected $fillable = [
         'name', 'email',
     ];
@@ -30,4 +31,26 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    // Rest omitted for brevity
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
