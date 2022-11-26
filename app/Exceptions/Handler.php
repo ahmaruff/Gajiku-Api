f66cd32a058f0461b6d94fdfdba206438f58c58e
ahmaruff <ahmadmaruf2701@gmail.com>
2022-11-26 12:36:24 +0700

    create custom response handler in render() function

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,14 @@
 
 namespace App\Exceptions;
 
+use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -49,6 +52,27 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        // return parent::render($request, $exception);
+        $rendered = parent::render($request, $exception);
+
+
+        if($exception instanceof NotFoundHttpException){
+            $message = $exception->getMessage() ? $exception->getMessage() : Response::$statusTexts[$rendered->getStatusCode()];;
+            $exception = new NotFoundHttpException($message,$exception);
+        }elseif($exception instanceof HttpException) {
+            $message = $exception->getMessage() ? $exception->getMessage() : Response::$statusTexts[$rendered->getStatusCode()];
+            $exception = new HttpException($message, $exception);
+        }else {
+            $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+            $message = env('APP_DEBUG', false) ? $exception->getMessage() : Response::$statusTexts[$statusCode];
+            $exception = new HttpException($statusCode, $message);
+        }
+
+        // response
+        return response()->json([
+            'status' => 'error',
+            'code' => $rendered->getStatusCode(),
+            'message' => $exception->getMessage()
+        ],$rendered->getStatusCode());
     }
 }
